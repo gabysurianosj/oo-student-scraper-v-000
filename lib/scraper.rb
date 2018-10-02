c65621e014a326a7600e8305cbf087b93caad053
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-02 22:10:03 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,6 +11,10 @@ class Scraper
     	        student_profile_link = "#{student.attr('href')}"
     	        student_location = student.css('.student-location').text
          student_name = student.css('.student-name').text
+         students << {name: student_name, location: student_location, profile_url: student_profile_link}
+       end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
