bf1982687646e398eff16a9c3b6665200a087e32
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-02 22:09:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ class Scraper
     index_page = Nokogiri::HTML(open(index_url))
     students = [] 
     index_page.css("div.roster-cards-container").each do |card|
-        
+        card.css(".student-card a").each do |student|
+    	        student_profile_link = "#{student.attr('href')}"
   end
 
   def self.scrape_profile_page(profile_url)
